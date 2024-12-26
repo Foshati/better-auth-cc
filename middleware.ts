13195a851a222892b1bc6eeaae20388b4e0d5814
Foshati <amirrezafoshati@gmail.com>
2024-12-26 22:46:13 +0330

    Add user role management and enhance authentication middleware

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,16 @@ import { Session } from "./app/(auth)/_lib/auth";
 const authRoutes = ["/sign-in", "/sign-up"];
 const passwordRoutes = ["/reset-password", "/forgot-password"];
 const adminRoutes = ["/admin"];
+const userRoutes = ["/dashboard"];
+const publicRoutes = [""];
 
 export default async function authMiddleware(request: NextRequest) {
   const pathName = request.nextUrl.pathname;
   const isAuthRoute = authRoutes.includes(pathName);
   const isPasswordRoute = passwordRoutes.includes(pathName);
   const isAdminRoute = adminRoutes.includes(pathName);
+  const isUserRoute = userRoutes.includes(pathName);
+  const isPublicRoute = publicRoutes.includes(pathName);
 
   const { data: session } = await betterFetch<Session>(
     "/api/auth/get-session",
@@ -20,7 +24,7 @@ export default async function authMiddleware(request: NextRequest) {
         //get the cookie from the request
         cookie: request.headers.get("cookie") || "",
       },
-    },
+    }
   );
 
   if (!session) {
@@ -37,10 +41,17 @@ export default async function authMiddleware(request: NextRequest) {
   if (isAdminRoute && session.user.role !== "admin") {
     return NextResponse.redirect(new URL("/", request.url));
   }
+  if (isUserRoute && session.user.role !== "user") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (isPublicRoute) {
+    return NextResponse.next();
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
